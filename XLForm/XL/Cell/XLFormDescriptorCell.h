0f616b9d4ebbea03b01293445eb5faba9f6df4f1
Fabian Pahl <fabian@21stdigital.de>
2014-09-05 18:58:28 +0200

    Fixed 'Expected a type' compiler error

--- XLForm/XL/Cell/XLFormDescriptorCell.h
+++ XLForm/XL/Cell/XLFormDescriptorCell.h
@@ -23,7 +23,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 
 @class XLFormRowDescriptor;
