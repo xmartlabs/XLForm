b8868af9cfdd8c132d888ee89a613d8ff6d56bf0
Borba <gaston@xmartlabs.com>
2015-03-16 17:03:24 -0300

    add Float Labeled Text Field Cell

---
+++ Examples/Objective-C/Examples/CustomRows/FloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.h
@@ -0,0 +1,116 @@
+//
+//  JVFloatLabeledTextField.h
+//  JVFloatLabeledTextField
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2013-2015 Jared Verdi
+//  Original Concept by Matt D. Smith
+//  http://dribbble.com/shots/1254439--GIF-Mobile-Form-Interaction?list=users
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import <UIKit/UIKit.h>
+
+/**
+ * `JVFloatLabeledTextField` is a `UITextField` subclass that implements the "Float Label Pattern".
+ *
+ * Due to space constraints on mobile devices, it is common to rely solely on placeholders as a means to label fields.
+ * This presents a UX problem, in that, once the user begins to fill out a form, no labels are present.
+ *
+ * `JVFloatLabeledTextField` aims to improve the user experience by having placeholders transition into 
+ * "floating labels" that hover above the text field after it is populated with text.
+ *
+ * JVFloatLabeledTextField supports iOS 6+.
+ *
+ * Credits for the concept to Matt D. Smith (@mds), and his original design:  http://mattdsmith.com/float-label-pattern/
+ */
+@interface JVFloatLabeledTextField : UITextField
+
+/**
+ * Read-only access to the floating label.
+ */
+@property (nonatomic, strong, readonly) UILabel * floatingLabel;
+
+/**
+ * Padding to be applied to the y coordinate of the floating label upon presentation.
+ * Defaults to zero.
+ */
+@property (nonatomic) CGFloat floatingLabelYPadding UI_APPEARANCE_SELECTOR;
+
+/**
+ * Padding to be applied to the y coordinate of the placeholder.
+ * Defaults to zero.
+ */
+@property (nonatomic) CGFloat placeholderYPadding UI_APPEARANCE_SELECTOR;
+
+/**
+ * Font to be applied to the floating label. 
+ * Defaults to `[UIFont boldSystemFontOfSize:12.0f]`.
+ * Provided for the convenience of using as an appearance proxy.
+ */
+@property (nonatomic, strong) UIFont * floatingLabelFont UI_APPEARANCE_SELECTOR;
+
+/**
+ * Text color to be applied to the floating label. 
+ * Defaults to `[UIColor grayColor]`.
+ * Provided for the convenience of using as an appearance proxy.
+ */
+@property (nonatomic, strong) UIColor * floatingLabelTextColor UI_APPEARANCE_SELECTOR;
+
+/**
+ * Text color to be applied to the floating label while the field is a first responder.
+ * Tint color is used by default if an `floatingLabelActiveTextColor` is not provided.
+ */
+@property (nonatomic, strong) UIColor * floatingLabelActiveTextColor UI_APPEARANCE_SELECTOR;
+
+/**
+ * Indicates whether the floating label's appearance should be animated regardless of first responder status.
+ * By default, animation only occurs if the text field is a first responder.
+ * Non-zero values indicate truthiness. (BOOL cannot be used with UI_APPEARANCE_SELECTOR on iOS 7).
+ */
+@property (nonatomic, assign) NSInteger animateEvenIfNotFirstResponder UI_APPEARANCE_SELECTOR;
+
+/**
+ * Duration of the animation when showing the floating label. 
+ * Defaults to 0.3 seconds.
+ */
+@property (nonatomic, assign) NSTimeInterval floatingLabelShowAnimationDuration UI_APPEARANCE_SELECTOR;
+
+/**
+ * Duration of the animation when hiding the floating label. 
+ * Defaults to 0.3 seconds.
+ */
+@property (nonatomic, assign) NSTimeInterval floatingLabelHideAnimationDuration UI_APPEARANCE_SELECTOR;
+
+/**
+ * Indicates whether the clearButton position is adjusted to align with the text
+ * Defaults to 1.
+ * Non-zero values indicate truthiness. (BOOL cannot be used with UI_APPEARANCE_SELECTOR on iOS 7).
+ */
+@property (nonatomic, assign) NSInteger adjustsClearButtonRect UI_APPEARANCE_SELECTOR;
+
+/**
+ *  Sets the placeholder and the floating title
+ *
+ *  @param placeholder The string that to be shown in the text field when no other text is present.
+ *  @param floatingTitle The string to be shown above the text field once it has been populated with text by the user.
+ */
+- (void)setPlaceholder:(NSString *)placeholder floatingTitle:(NSString *)floatingTitle;
+
+@end
---
+++ Examples/Objective-C/Examples/CustomRows/FloatLabeledTextField/JVFloatLabeledTextField/JVFloatLabeledTextField.m
@@ -0,0 +1,264 @@
+//
+//  JVFloatLabeledTextField.m
+//  JVFloatLabeledTextField
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2013-2015 Jared Verdi
+//  Original Concept by Matt D. Smith
+//  http://dribbble.com/shots/1254439--GIF-Mobile-Form-Interaction?list=users
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in
+//  the Software without restriction, including without limitation the rights to
+//  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//  the Software, and to permit persons to whom the Software is furnished to do so,
+//  subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "JVFloatLabeledTextField.h"
+#import "NSString+TextDirectionality.h"
+
+#define kFloatingLabelShowAnimationDuration 0.3f
+#define kFloatingLabelHideAnimationDuration 0.3f
+
+@implementation JVFloatLabeledTextField
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit
+{
+    _floatingLabel = [UILabel new];
+    _floatingLabel.alpha = 0.0f;
+    [self addSubview:_floatingLabel];
+	
+    // some basic default fonts/colors
+    _floatingLabelFont = [UIFont boldSystemFontOfSize:12.0f];
+    _floatingLabel.font = _floatingLabelFont;
+    _floatingLabelTextColor = [UIColor grayColor];
+    _floatingLabel.textColor = _floatingLabelTextColor;
+    _animateEvenIfNotFirstResponder = 0;
+    _floatingLabelShowAnimationDuration = kFloatingLabelShowAnimationDuration;
+    _floatingLabelHideAnimationDuration = kFloatingLabelHideAnimationDuration;
+    [self setFloatingLabelText:self.placeholder];
+    
+    _adjustsClearButtonRect = 1;
+}
+
+#pragma mark -
+
+- (UIColor *)labelActiveColor
+{
+    if (_floatingLabelActiveTextColor) {
+        return _floatingLabelActiveTextColor;
+    }
+    else if ([self respondsToSelector:@selector(tintColor)]) {
+        return [self performSelector:@selector(tintColor)];
+    }
+    return [UIColor blueColor];
+}
+
+- (void)setFloatingLabelFont:(UIFont *)floatingLabelFont
+{
+    _floatingLabelFont = floatingLabelFont;
+    _floatingLabel.font = (_floatingLabelFont ? _floatingLabelFont : [UIFont boldSystemFontOfSize:12.0f]);
+    [self setFloatingLabelText:self.placeholder];
+    [self invalidateIntrinsicContentSize];
+}
+
+- (void)showFloatingLabel:(BOOL)animated
+{
+    void (^showBlock)() = ^{
+        _floatingLabel.alpha = 1.0f;
+        _floatingLabel.frame = CGRectMake(_floatingLabel.frame.origin.x,
+                                          _floatingLabelYPadding,
+                                          _floatingLabel.frame.size.width,
+                                          _floatingLabel.frame.size.height);
+    };
+    
+    if (animated || 0 != _animateEvenIfNotFirstResponder) {
+        [UIView animateWithDuration:_floatingLabelShowAnimationDuration
+                              delay:0.0f
+                            options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseOut
+                         animations:showBlock
+                         completion:nil];
+    }
+    else {
+        showBlock();
+    }
+}
+
+- (void)hideFloatingLabel:(BOOL)animated
+{
+    void (^hideBlock)() = ^{
+        _floatingLabel.alpha = 0.0f;
+        _floatingLabel.frame = CGRectMake(_floatingLabel.frame.origin.x,
+                                          _floatingLabel.font.lineHeight + _placeholderYPadding,
+                                          _floatingLabel.frame.size.width,
+                                          _floatingLabel.frame.size.height);
+
+    };
+    
+    if (animated || 0 != _animateEvenIfNotFirstResponder) {
+        [UIView animateWithDuration:_floatingLabelHideAnimationDuration
+                              delay:0.0f
+                            options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseIn
+                         animations:hideBlock
+                         completion:nil];
+    }
+    else {
+        hideBlock();
+    }
+}
+
+- (void)setLabelOriginForTextAlignment
+{
+    CGRect textRect = [self textRectForBounds:self.bounds];
+    
+    CGFloat originX = textRect.origin.x;
+    
+    if (self.textAlignment == NSTextAlignmentCenter) {
+        originX = textRect.origin.x + (textRect.size.width/2) - (_floatingLabel.frame.size.width/2);
+    }
+    else if (self.textAlignment == NSTextAlignmentRight) {
+        originX = textRect.origin.x + textRect.size.width - _floatingLabel.frame.size.width;
+    }
+    else if (self.textAlignment == NSTextAlignmentNatural) {
+        JVTextDirection baseDirection = [_floatingLabel.text getBaseDirection];
+        if (baseDirection == JVTextDirectionRightToLeft) {
+            originX = textRect.origin.x + textRect.size.width - _floatingLabel.frame.size.width;
+        }
+    }
+    
+    _floatingLabel.frame = CGRectMake(originX, _floatingLabel.frame.origin.y,
+                                      _floatingLabel.frame.size.width, _floatingLabel.frame.size.height);
+}
+
+- (void)setFloatingLabelText:(NSString *)text
+{
+    _floatingLabel.text = text;
+    [self setNeedsLayout];
+}
+
+#pragma mark - UITextField
+
+- (CGSize)intrinsicContentSize
+{
+    CGSize textFieldIntrinsicContentSize = [super intrinsicContentSize];
+    return CGSizeMake(textFieldIntrinsicContentSize.width,
+                      textFieldIntrinsicContentSize.height + _floatingLabelYPadding + _floatingLabel.font.lineHeight);
+}
+
+- (void)setPlaceholder:(NSString *)placeholder
+{
+    [super setPlaceholder:placeholder];
+    [self setFloatingLabelText:placeholder];
+}
+
+- (void)setAttributedPlaceholder:(NSAttributedString *)attributedPlaceholder
+{
+    [super setAttributedPlaceholder:attributedPlaceholder];
+    [self setFloatingLabelText:attributedPlaceholder.string];
+}
+
+- (void)setPlaceholder:(NSString *)placeholder floatingTitle:(NSString *)floatingTitle
+{
+    [super setPlaceholder:placeholder];
+    [self setFloatingLabelText:floatingTitle];
+}
+
+- (CGRect)textRectForBounds:(CGRect)bounds
+{
+    CGRect rect = [super textRectForBounds:bounds];
+    if ([self.text length]) {
+        CGFloat topInset = ceilf(_floatingLabel.font.lineHeight + _placeholderYPadding);
+        topInset = MIN(topInset, [self maxTopInset]);
+        rect = UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(topInset, 0.0f, 0.0f, 0.0f));
+    }
+    return CGRectIntegral(rect);
+}
+
+- (CGRect)editingRectForBounds:(CGRect)bounds
+{
+    CGRect rect = [super editingRectForBounds:bounds];
+    if ([self.text length]) {
+        CGFloat topInset = ceilf(_floatingLabel.font.lineHeight + _placeholderYPadding);
+        topInset = MIN(topInset, [self maxTopInset]);
+        rect = UIEdgeInsetsInsetRect(rect, UIEdgeInsetsMake(topInset, 0.0f, 0.0f, 0.0f));
+    }
+    return CGRectIntegral(rect);
+}
+
+- (CGRect)clearButtonRectForBounds:(CGRect)bounds
+{
+    CGRect rect = [super clearButtonRectForBounds:bounds];
+    if (0 != self.adjustsClearButtonRect) {
+        if ([self.text length]) {
+            CGFloat topInset = ceilf(_floatingLabel.font.lineHeight + _placeholderYPadding);
+            topInset = MIN(topInset, [self maxTopInset]);
+            rect = CGRectMake(rect.origin.x, rect.origin.y + topInset / 2.0f, rect.size.width, rect.size.height);
+        }
+    }
+    return CGRectIntegral(rect);
+}
+
+- (CGFloat)maxTopInset
+{
+    return MAX(0, floorf(self.bounds.size.height - self.font.lineHeight - 4.0f));
+}
+
+- (void)setTextAlignment:(NSTextAlignment)textAlignment
+{
+    [super setTextAlignment:textAlignment];
+    [self setNeedsLayout];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    [self setLabelOriginForTextAlignment];
+    
+    if (self.floatingLabelFont) {
+        _floatingLabel.font = self.floatingLabelFont;
+    }
+    
+    [_floatingLabel sizeToFit];
+    
+    BOOL firstResponder = self.isFirstResponder;
+    _floatingLabel.textColor = (firstResponder && self.text && self.text.length > 0 ?
+                                self.labelActiveColor : self.floatingLabelTextColor);
+    if (!self.text || 0 == [self.text length]) {
+        [self hideFloatingLabel:firstResponder];
+    }
+    else {
+        [self showFloatingLabel:firstResponder];
+    }
+}
+
+@end
