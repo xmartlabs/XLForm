33bd5ebf8a7facc0c07544f6311631502a22a3a6
Borba <gaston@xmartlabs.com>
2015-03-25 11:39:28 -0300

    add XLFormExampleTest

---
+++ Tests/XLForm Tests/Helpers/UITextField+Test.h
@@ -0,0 +1,16 @@
+//
+//  UITextField+Test.h
+//  XLForm Tests
+//
+//  Created by Gaston Borba on 3/25/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (Test)
+
+// This category is for simulate the change of the text
+-(void)changeText:(NSString *)string;
+
+@end
