e7e3ac63b0e8bf3caaffc127ec9f8885e278b72e
Colm Doyle <colm@kitmanlabs.com>
2015-04-09 15:01:37 +0100

    Autolayout changes

--- XLForm/XL/Cell/XLFormTextFieldCell.m
+++ XLForm/XL/Cell/XLFormTextFieldCell.m
@@ -181,11 +181,16 @@
 {
     NSMutableArray * result = [[NSMutableArray alloc] init];
     [self.textLabel setContentHuggingPriority:500 forAxis:UILayoutConstraintAxisHorizontal];
-    [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[_textLabel]-[_textField]" options:NSLayoutFormatAlignAllBaseline metrics:0 views:NSDictionaryOfVariableBindings(_textLabel, _textField)]];
+
+  // When the textField doesn't have a minimum width, the label clobbers it in autolayout.
+  // This solution works for us, but might not suit XLForm, so I won't open a PR upstream until I chat with them.
+    [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[_textLabel]-[_textField(>=60)]" options:NSLayoutFormatAlignAllBaseline metrics:0 views:NSDictionaryOfVariableBindings(_textLabel, _textField)]];
     
     [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=11)-[_textField]-(>=11)-|" options:NSLayoutFormatAlignAllBaseline metrics:nil views:NSDictionaryOfVariableBindings(_textField)]];
-    
-    [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=11)-[_textLabel]-(>=11)-|" options:NSLayoutFormatAlignAllBaseline metrics:nil views:NSDictionaryOfVariableBindings(_textLabel)]];
+  // With a constraint of >=11, our chosen font size gets cut off vertically.
+  // This solution works for us, but might not suit XLForm, so I won't open a PR upstream until I chat with them.
+
+  //    [result addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=11)-[_textLabel]-(>=11)-|" options:NSLayoutFormatAlignAllBaseline metrics:nil views:NSDictionaryOfVariableBindings(_textLabel)]];
     return result;
 }
 
