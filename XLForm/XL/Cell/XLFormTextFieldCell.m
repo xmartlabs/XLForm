6ef57913b2c2f912b2839d4d13928266073c5a12
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2015-12-22 11:06:34 +0100

    Add locale-specific decimal handling with XLFormRowDescriptorTypeDecimal

--- XLForm/XL/Cell/XLFormTextFieldCell.m
+++ XLForm/XL/Cell/XLFormTextFieldCell.m
@@ -261,7 +261,10 @@
 - (void)textFieldDidChange:(UITextField *)textField{
     if([self.textField.text length] > 0) {
         if ([self.rowDescriptor.rowType isEqualToString:XLFormRowDescriptorTypeNumber] || [self.rowDescriptor.rowType isEqualToString:XLFormRowDescriptorTypeDecimal]){
-            self.rowDescriptor.value =  @([self.textField.text doubleValue]);
+	    NSNumberFormatter * myNumFormatter = [[NSNumberFormatter alloc] init];
+	    [myNumFormatter setLocale:[NSLocale currentLocale]];
+	    [myNumFormatter setNumberStyle:NSNumberFormatterDecimalStyle];
+	    self.rowDescriptor.value =  [myNumFormatter numberFromString:self.textField.text];
         } else if ([self.rowDescriptor.rowType isEqualToString:XLFormRowDescriptorTypeInteger]){
             self.rowDescriptor.value = @([self.textField.text integerValue]);
         } else {
