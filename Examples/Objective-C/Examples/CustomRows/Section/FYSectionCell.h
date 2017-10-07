c81d5f0617457d650ac0a16ebc093336ea08f7fa
felixyin <ybkk1027@gmail.com>
2017-10-07 14:09:01 +0800

    自定义表头表尾

---
+++ Examples/Objective-C/Examples/CustomRows/Section/FYSectionCell.h
@@ -0,0 +1,13 @@
+//
+//  FYSectionCell.h
+//  XLForm
+//
+//  Created by 尹彬 on 2017/10/7.
+//  Copyright © 2017年 Xmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FYSectionCell : UITableViewCell
+
+@end
---
+++ Examples/Objective-C/Examples/CustomRows/Section/FYSectionCell.m
@@ -0,0 +1,24 @@
+//
+//  FYSectionCell.m
+//  XLForm
+//
+//  Created by 尹彬 on 2017/10/7.
+//  Copyright © 2017年 Xmartlabs. All rights reserved.
+//
+
+#import "FYSectionCell.h"
+
+@implementation FYSectionCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
