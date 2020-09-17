56476ca4a39af0fe07289493b3ac66f93ff1d1d2
Eneko Alonso <eneko.alonso@gmail.com>
2020-09-17 10:22:09 -0700

    Clean up

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
@@ -15,23 +14,8 @@ let package = Package(
         .target(
             name: "XLForm",
             dependencies: [],
-
-            // Required to build package
             path: "XLForm",
-
-            // Required for Xcode to find import header when building app
-            publicHeadersPath: "XL"//,
-
-            // Required to build package
-//            cSettings: [
-//                .headerSearchPath("XL"),
-//                .headerSearchPath("XL/Cell"),
-//                .headerSearchPath("XL/Controllers"),
-//                .headerSearchPath("XL/Descriptors"),
-//                .headerSearchPath("XL/Helpers"),
-//                .headerSearchPath("XL/Helpers/Views"),
-//                .headerSearchPath("XL/Validation"),
-//            ]
+            publicHeadersPath: "XL"
         )
     ]
 )
